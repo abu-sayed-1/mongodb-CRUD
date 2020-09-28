00ea77af048fa28bad10b411b65028ff3b13214a
as ahmed <rakibasrr13@gmail.com>
2020-09-28 22:45:29 +0300

    save data

--- index.js
+++ index.js
@@ -26,28 +26,65 @@
 
 
 
+  //  Module No 48/video No 04
+  // const express = require('express');
+  // const MongoClient = require('mongodb').MongoClient;
+  // const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
+
+  // const app = express();
+
+  // app.get('/', (req, res) =>{
+  //   res.sendFile(__dirname + '/index.html');
+  //  })
+
+  // client.connect(err => {
+  //   const collection = client.db("projectbd").collection("products");
+  //   app.post("/addProduct", (req, res) => {
+      
+  //     collection.insertOne(product)
+  //     .then(result => {
+  //       console.log('one product added');
+  //     })
+  //   })
+    
+  //   console.log(' database collection habizabi');
+  //   // client.close();
+  // });
+
+  // app.listen(3200)
+
+
+
+  //-- Module No 48/video No 05--save data in database and Read all data
   const express = require('express');
+  const bodyParser = require('body-parser');
   const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
 
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:false}));
 
   app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/index.html');
    })
 
+
+
   client.connect(err => {
-    const collection = client.db("projectbd").collection("products");
+    const productCollection = client.db("projectbd").collection("products");
     app.post("/addProduct", (req, res) => {
-      
-      collection.insertOne(product)
+      const product = req.body;
+      // console.log(product);
+      productCollection.insertOne(product)
       .then(result => {
-        console.log('one product added');
+        console.log('data added successfully');
+        res.send('succuss')
       })
+      
     })
-    
-    console.log(' database collection habizabi');
     // client.close();
   });
 
