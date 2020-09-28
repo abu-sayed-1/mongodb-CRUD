8e599c41bc9c117f2abe36505c31a67b8e98ef5f
as ahmed <rakibasrr13@gmail.com>
2020-09-29 02:10:16 +0300

    delete-mathod

--- index.js
+++ index.js
@@ -57,12 +57,100 @@
 
 
   //-- Module No 48/video No 05--save data in database and Read all data
+  // const express = require('express');
+  // const bodyParser = require('body-parser');
+  // const MongoClient = require('mongodb').MongoClient;
+  // const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
+
+  // const app = express();
+  // app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({extended:false}));
+
+  // app.get('/', (req, res) =>{
+  //   res.sendFile(__dirname + '/index.html');
+  //  })
+
+
+
+  // client.connect(err => {
+  //   const productCollection = client.db("projectbd").collection("products");
+  //   app.post("/addProduct", (req, res) => {
+  //     const product = req.body;
+  //     // console.log(product);
+  //     productCollection.insertOne(product)
+  //     .then(result => {
+  //       console.log('data added successfully');
+  //       res.send('succuss')
+  //     })
+      
+  //   })
+  //   // client.close();
+  // });
+
+  // app.listen(3200)
+
+
+
+
+  //    Module No 48/video No 06--load data database data and show on the UI
+  // const express = require('express');
+  // const bodyParser = require('body-parser');
+  // const MongoClient = require('mongodb').MongoClient;
+  // const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
+
+  // const app = express();
+  // app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({extended:false}));
+
+  // app.get('/', (req, res) =>{
+  //   res.sendFile(__dirname + '/index.html');
+  //  })
+
+
+
+  // client.connect(err => {
+  //   const productCollection = client.db("projectbd").collection("products");
+
+  //   app.get('/products',(req, res) => {
+  //     productCollection.find({}).limit(2)
+  //   .toArray( (err, documents) => {
+  //     res.send(documents);
+  //   })
+
+  //   })
+
+
+
+
+  //   app.post("/addProduct", (req, res) => {
+  //     const product = req.body;
+  //     // console.log(product);
+  //     productCollection.insertOne(product)
+  //     .then(result => {
+  //       console.log('data added successfully');
+  //       res.send('succuss')
+  //     })
+      
+  //   })
+  //   // client.close();
+  // });
+
+  // app.listen(3100)
+
+
+
+
+
+
+  //---- Module No 48/video No 07----delete an item from the database from UI---
   const express = require('express');
   const bodyParser = require('body-parser');
   const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
-
+  const ObjectId = require('mongodb').ObjectId
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:false}));
@@ -75,6 +163,18 @@
 
   client.connect(err => {
     const productCollection = client.db("projectbd").collection("products");
+
+    app.get('/products',(req, res) => {
+      productCollection.find({}).limit(4)
+    .toArray( (err, documents) => {
+      res.send(documents);
+    })
+
+    })
+
+
+
+
     app.post("/addProduct", (req, res) => {
       const product = req.body;
       // console.log(product);
@@ -87,5 +187,13 @@
     })
     // client.close();
   });
+  app.delete('/delete/:id', (req, res) =>{
+    console.log(req.params.id);
+    productCollection.deleteOne({_id:req.params.id})
+    .then (result => {
+      console.log(result)
+    }
+  })
 
-  app.listen(3200)
+  app.listen(3100)
+
