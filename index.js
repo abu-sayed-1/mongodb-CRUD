071b1513ddebc350f684fa8c43b2bdbbe6cb7dce
as ahmed <rakibasrr13@gmail.com>
2020-09-29 11:29:07 +0300

    mondodb

--- index.js
+++ index.js
@@ -145,12 +145,80 @@
 
 
   //---- Module No 48/video No 07----delete an item from the database from UI---
+  // const express = require('express');
+  // const bodyParser = require('body-parser');
+  // const MongoClient = require('mongodb').MongoClient;
+  // const ObjectId = require('mongodb').ObjectId
+  // // server ^^ theke delete hoiteche na ei karone ObjectId import kora lagche..
+  // const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
+  // // const ObjectId = require('mongodb').ObjectId
+  // const app = express();
+  // app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({extended:false}));
+
+  // app.get('/', (req, res) =>{
+  //   res.sendFile(__dirname + '/index.html');
+  //  })
+
+
+
+  // client.connect(err => {
+  //   const productCollection = client.db("projectbd").collection("products");
+
+  //   app.get('/products',(req, res) => {
+  //     productCollection.find({}).limit(4)
+  //   .toArray( (err, documents) => {
+  //     res.send(documents);
+  //   })
+
+  //   })
+
+
+
+
+  //   app.post("/addProduct", (req, res) => {
+  //     const product = req.body;
+  //     // console.log(product);
+  //     productCollection.insertOne(product)
+    
+  //     .then(result => {
+  //       console.log('data added successfully');
+  //       res.send('succuss')
+  //     })
+  //       deleteId(productCollection)
+  //   })
+  //   // client.close();
+  // });
+
+  // const deleteId = product => {
+  //   app.delete('/delete/:id', (req, res) => {
+  //     // console.log(req.params.id);
+  //    product.deleteOne({_id: ObjectId(req.params.id)})
+  //     .then (result => {
+  //       console.log(result)
+  //     })
+  
+    
+  //   })
+  // }
+ 
+   
+ 
+
+  // app.listen(3100)
+
+
+
+//---- Module No 48/video No 08----load single product from database using id
   const express = require('express');
   const bodyParser = require('body-parser');
   const MongoClient = require('mongodb').MongoClient;
+  const ObjectId = require('mongodb').ObjectId
+  // server ^^ theke delete hoiteche na ei karone ObjectId import kora lagche..
   const uri = "mongodb+srv://rakib:lkfjlak7683@cluster0.uj2jz.mongodb.net/rakib?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
-  const ObjectId = require('mongodb').ObjectId
+  // const ObjectId = require('mongodb').ObjectId
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:false}));
@@ -165,10 +233,21 @@
     const productCollection = client.db("projectbd").collection("products");
 
     app.get('/products',(req, res) => {
-      productCollection.find({}).limit(4)
+      productCollection.find({})
+      // .limit(4)
     .toArray( (err, documents) => {
       res.send(documents);
     })
+
+    })
+
+    // single product  -----
+    app.get('/products/:id', (req, res) => {
+    //  const num = deleteId(productCollection)
+    productCollection.find({_id: ObjectId(req.params.id)})
+      .toArray ((err, documents) => {
+        res.send(documents[0]);
+      })
 
     })
 
@@ -179,21 +258,30 @@
       const product = req.body;
       // console.log(product);
       productCollection.insertOne(product)
+    
       .then(result => {
         console.log('data added successfully');
         res.send('succuss')
       })
-      
+        deleteId(productCollection)
     })
     // client.close();
   });
-  app.delete('/delete/:id', (req, res) =>{
-    console.log(req.params.id);
-    productCollection.deleteOne({_id:req.params.id})
-    .then (result => {
-      console.log(result)
-    }
-  })
+
+  const deleteId = product => {
+    app.delete('/delete/:id', (req, res) => {
+      // console.log(req.params.id);
+     product.deleteOne({_id: ObjectId(req.params.id)})
+      .then (result => {
+        console.log(result)
+      })
+  
+    
+    })
+  }
+ 
+   
+ 
 
   app.listen(3100)
 
